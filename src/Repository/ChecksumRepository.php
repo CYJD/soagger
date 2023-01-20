78e2a886e93f4a8397c085e18829baec409e26cf
EDDA France <99742331+CYJD@users.noreply.github.com>
2023-01-20 15:15:22 +0100

    Add files via upload

---
+++ src/Repository/ChecksumRepository.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Checksum;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+class CarDataRepository extends ServiceEntityRepository
+{
+	public function findChecksum()
+	{
+		return $this->createQueryBuilder('c')
+            ->select()
+			->orderBy('c.date', 'DESC')
+            ->getQuery()
+            ->getResult()
+        ;
+	}
+}
