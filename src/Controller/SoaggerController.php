809306d29bd55a8b7f948feeb0128c2db6343001
EDDA France <99742331+CYJD@users.noreply.github.com>
2023-01-20 15:17:10 +0100

    Add files via upload

---
+++ src/Controller/SoaggerController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SoaggerController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+		$wsdl = "http://atlas-prep-se.cartercash.com:10000/catalogue.svc?wsdl";
+		
+		$file = fopen($wsdl, 'r');
+		
+		while(! feof($file)) {
+			$line = fgets($file);
+		}
+		dump($line);
+		fclose($file);
+
+		$crc = crc32($line);
+		dump($crc);
+		
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'SoaggerController',
+        ]);
+    }
+}
